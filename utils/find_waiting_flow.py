4a6a2c4d00a8f1941619d7c1e173cbcb2bffc8de
Kevin Coakley <kcoakley@sdsc.edu>
2015-04-28 14:07:00 -0700

    Don't print AWS key_id and secret_key

--- utils/find_waiting_flow.py
+++ utils/find_waiting_flow.py
@@ -1,6 +1,6 @@
 from mrjob.emr import EMRJobRunner
 def find_waiting_flow(aws_access_key_id,aws_secret_access_key,ssh_key_pair_file=''):
-    print (aws_access_key_id,aws_secret_access_key)
+    # print (aws_access_key_id,aws_secret_access_key)
     JobRunner = EMRJobRunner(aws_access_key_id=aws_access_key_id,aws_secret_access_key=aws_secret_access_key)
     emr_conn = JobRunner.make_emr_conn()
     job_flows=emr_conn.describe_jobflows()
